3a3cd851203cc3fbc2a527ac12fb732e8500cbb5
davhor <david.hornfeck@zenuity.com>
2020-04-03 17:30:40 +0200

    Add comments to current main for documentation

--- src/Arduino_PS.cpp
+++ src/Arduino_PS.cpp
@@ -22,7 +22,6 @@ bool water_on = false;
 bool timer_on = false;
 bool water_level_ok = false;
 
-
 WaterSystem PumpControl;
 
 DS3231 clock_var;
@@ -30,12 +29,12 @@ RTCDateTime DateTime;
 
 bool timer_water_flag = true;
 
-int main(void) //Enable for Testing
-{ // Enable for Testing
-for(int i = 0; i < 2; i++) //enable for Testing
-{ //enable for Testing
+int main(void)                //Enable for Testing
+{                             // Enable for Testing
+  for (int i = 0; i < 2; i++) //enable for Testing
+  {                           //enable for Testing
 
-/*
+    /*
 void setup() // Enable on Hardware
 {
 
@@ -53,80 +52,84 @@ void setup() // Enable on Hardware
   //clock.setDateTime(2019, 10, 27, 8, 42, 15);
   */
 
-  digitalWrite(PUMP, HIGH); //default no pumping enabled
-  digitalWrite(VALVETOP, HIGH); //default no pumping enabled
-  digitalWrite(VALVEBOTTOM, HIGH); //default no pumping enabled
+    digitalWrite(PUMP, HIGH);        //default no pumping enabled
+    digitalWrite(VALVETOP, HIGH);    //default no pumping enabled
+    digitalWrite(VALVEBOTTOM, HIGH); //default no pumping enabled
 
-  //prevent different states when water switch is on
-  if (digitalRead(WATER) == LOW)
-  {
-    pre_state_water = true;
-    current_state_water = true;
-  }
+    //prevent different states when water switch is on
+    if (digitalRead(WATER) == LOW)
+    {
+      pre_state_water = true;
+      current_state_water = true;
+    }
 
-  Serial.println("\n\nStart of Program:");
-  Serial.println("-----------------\n");
-//} //Enable on Hardware
+    Serial.println("\n\nStart of Program:");
+    Serial.println("-----------------\n");
+    //} //Enable on Hardware
 
+    unsigned long counter = 0;
+    TIME t_min(0, 1), t1(0, 0), t2(0, 0), t_buffer(0, 0);
 
-unsigned long counter = 0;
-TIME t_min(0,1), t1(0,0), t2(0,0), t_buffer(0,0);
-
-/*
+    /*
 void loop() // Enable on Hardware
 { // Enable on Hardware
 */
-  counter++;
-  if( (counter%4000) == 0)
-  {
-    DateTime = clock_var.getDateTime();
-    Serial.print(DateTime.day);
-    Serial.print(".");
-    Serial.print(DateTime.month);
-    Serial.print(".");
-    Serial.print(DateTime.year);
-    Serial.print(": ");
-    Serial.print(DateTime.hour);
-    Serial.print(":");
-    Serial.print(DateTime.minute);
-    Serial.print("   ");
-    Serial.print("Temperature = ");
-    Serial.print(PumpControl.getTemperature());
-    Serial.println(" Celsius");
-  }
+    counter++;
+    if ((counter % 4000) == 0)
+    {
+      DateTime = clock_var.getDateTime();
+      Serial.print(DateTime.day);
+      Serial.print(".");
+      Serial.print(DateTime.month);
+      Serial.print(".");
+      Serial.print(DateTime.year);
+      Serial.print(": ");
+      Serial.print(DateTime.hour);
+      Serial.print(":");
+      Serial.print(DateTime.minute);
+      Serial.print("   ");
+      Serial.print("Temperature = ");
+      Serial.print(PumpControl.getTemperature());
+      Serial.println(" Celsius");
+    }
 
-  
-  //configuration settings - change time here
-  timer_on = true; // software switch for pump timer function
+    //configuration settings - change time here
+    timer_on = true; // software switch for pump timer function
 
-  // state logics
-  switch_on = PumpControl.isSystemSwitchedOn();
-  water_on = PumpControl.isWaterActivated();
+    // state logics
+    switch_on = PumpControl.isSystemSwitchedOn();
+    water_on = PumpControl.isWaterActivated();
 
-  // function execution
-  if (switch_on && water_on)
-  {
-    Serial.println("pumpWater single Function!");
-    PumpControl.pumpWater(par::t_half_can, VALVETOP, par::t_valve);
-    //PumpControl.pumpWater(t_bottom_vol, VALVEBOTTOM, t_valve);
-  }
+    // Manual watering
+    // applied only to valve for top plants since there is
+    // the manual tube attached for which this feature is designed
+    if (switch_on && water_on)
+    {
+      Serial.println("pumpWater single Function!");
+      PumpControl.pumpWater(par::t_half_can, VALVETOP, par::t_valve);
+    }
 
-  if (timer_on &&  ((PumpControl.getCurrentLocalTime() == par::t1_water) || (PumpControl.getCurrentLocalTime() == par::t2_water)) && timer_water_flag)
-  {
-    PumpControl.pumpWaterClock();
-    timer_water_flag = false;
-  }
+    // Watering according to timer
+    // timer_water_flag needed since the clock has resolution of 1 min
+    // but watering should happen only once during that minute
+    if (timer_on && ((PumpControl.getCurrentLocalTime() == par::t1_water) || (PumpControl.getCurrentLocalTime() == par::t2_water)) && timer_water_flag)
+    {
+      PumpControl.pumpWaterClock();
+      timer_water_flag = false;
+    }
 
-  
-  t_buffer = par::t1_water;
-  t1 = t_buffer + t_min;
-  t_buffer = par::t2_water;
-  t2 = t_buffer + t_min;
+    // buffer needed because par::t1::water is const and overloading of
+    // + operator is is not defined for const
+    t_buffer = par::t1_water;
+    t1 = t_buffer + t_min;
+    t_buffer = par::t2_water;
+    t2 = t_buffer + t_min;
 
-  if( (PumpControl.getCurrentLocalTime() == t1) || (PumpControl.getCurrentLocalTime() == t2) )
-  {
-    timer_water_flag = true;
-  }
+    // reset timer_water_flag after the minute of watering is past
+    if ((PumpControl.getCurrentLocalTime() == t1) || (PumpControl.getCurrentLocalTime() == t2))
+    {
+      timer_water_flag = true;
+    }
 
   } // Enable for Testing
 }
