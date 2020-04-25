a4da98d91698b54e9ec8f07f4adc722cc88c171c
davhor <david.hornfeck@zenuity.com>
2020-04-25 14:33:07 +0200

    Lower parameter for temp2 and temperatures above

--- deployment/parameter.h
+++ deployment/parameter.h
@@ -19,19 +19,19 @@ const short TEMPERATURE = 6;
 //Watering parameters for top plants
 const float maxTemp = 55.0F;
 const unsigned int timeMaxTempTop = 60000;
-const unsigned int timeMaxTempBottom = 12000;
+const unsigned int timeMaxTempBottom = 11000;
 
 const float temp4 = 45.0F;
 const unsigned int timeTemp4Top = 40000;
-const unsigned int timeTemp4Bottom = 10000;
+const unsigned int timeTemp4Bottom = 9000;
 
 const float temp3 = 35.0F;
 const unsigned int timeTemp3Top = 30000;
-const unsigned int timeTemp3Bottom = 7000;
+const unsigned int timeTemp3Bottom = 6000;
 
 const float temp2 = 25.0F;
-const unsigned int timeTemp2Top = 20000;
-const unsigned int timeTemp2Bottom = 6000;
+const unsigned int timeTemp2Top = 15000;
+const unsigned int timeTemp2Bottom = 5000;
 
 const float lowTemp = 18.0F;
 const unsigned int timeLowTempTop = 8000;
