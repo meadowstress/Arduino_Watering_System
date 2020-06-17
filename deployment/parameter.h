1d10b33f98c168ce61e9bbd9171c853da6bcf4f7
davhor <david.hornfeck@zenuity.com>
2020-06-17 20:04:16 +0200

    Adapt parameter file
    * more water for bottom plants
    * more water for top plants until 25 degrees

--- deployment/parameter.h
+++ deployment/parameter.h
@@ -22,23 +22,23 @@ const byte CHIPSELECT = 10;  // SD-Card Pin for Arduino Uno
 // Watering parameters for top plants
 const float maxTemp                  = 55.0F;
 const unsigned int timeMaxTempTop    = 60000;
-const unsigned int timeMaxTempBottom = 10000;
+const unsigned int timeMaxTempBottom = 11000;
 
 const float temp4                  = 45.0F;
 const unsigned int timeTemp4Top    = 40000;
-const unsigned int timeTemp4Bottom = 8000;
+const unsigned int timeTemp4Bottom = 9000;
 
 const float temp3                  = 35.0F;
 const unsigned int timeTemp3Top    = 30000;
-const unsigned int timeTemp3Bottom = 5000;
+const unsigned int timeTemp3Bottom = 6000;
 
 const float temp2                  = 25.0F;
-const unsigned int timeTemp2Top    = 15000;
-const unsigned int timeTemp2Bottom = 4000;
+const unsigned int timeTemp2Top    = 17000;
+const unsigned int timeTemp2Bottom = 5000;
 
 const float lowTemp                  = 20.0F;
-const unsigned int timeLowTempTop    = 8000;
-const unsigned int timeLowTempBottom = 3000;
+const unsigned int timeLowTempTop    = 10000;
+const unsigned int timeLowTempBottom = 4000;
 
 // Water Times
 const TIME t1_water(11, 30);
